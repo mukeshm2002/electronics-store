3f37852678a561a26ce0911114e1c9bcb4ebee26
Mukesh <mukeshkannan509@gmail.com>
2026-06-09 16:06:40 +0530

    000

--- src/main/java/com/electricshop/electronics_store/controller/ProductController.java
+++ src/main/java/com/electricshop/electronics_store/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.electricshop.electronics_store.model.Product;
 import com.electricshop.electronics_store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -15,15 +16,26 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    // This will now correctly map to /admin/products/add
     @GetMapping("/add")
     public String showAddForm() {
         return "admin-add-product";
     }
 
+    // This will now correctly map to /admin/products/save
     @PostMapping("/save")
     public String saveProduct(@ModelAttribute Product product,
                               @RequestParam("file") MultipartFile file) throws IOException {
         productService.saveProduct(product, file);
-        return "redirect:/admin/products/products"; // Redirect to admin product list
+        return "redirect:/admin/products/products";
+    }
+
+    // THIS IS THE KEY CHANGE:
+    // Use an empty path "" or just "/products" depending on your preference.
+    // If you want the URL to be /admin/products/products, define it as below:
+    @GetMapping("/products")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products"; // Refers to products.html
     }
 }
